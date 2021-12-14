886889a32420c87146809bf9f24d0ee66272353e
AlonaPiasetskaDev <alona.piasetska.dev@gmail.com>
2021-12-14 17:53:24 +0200

    add api endpoints

--- src/api/api.js
+++ src/api/api.js
@@ -11,7 +11,35 @@ const post = async (endpoint, body) => {
   }
   return await res.json();
 };
-export { post };
+
+const get = async (endpoint) => {
+  const options = {
+    method: "GET",
+    headers: { ...defaults.headers },
+  };
+
+  const res = await fetch(`${defaults.host}/${endpoint}`, options);
+  if (!res.ok) {
+    throw new Error(`An error has occured: ${res.status}`);
+  }
+  return await res.json();
+};
+
+const put = async (endpoint, body) => {
+  const options = {
+    method: "PUT",
+    headers: { ...defaults.headers },
+  };
+
+  const res = await fetch(`${defaults.host}/${endpoint}`, options);
+  if (!res.ok) {
+    throw new Error(`An error has occured: ${res.status}`);
+  }
+
+  return await res.json();
+};
+
+export { post, put, get };
 export const defaults = {
   host: "http://localhost:4000",
   headers: {
